ea7bd9378e687ca396ef0b916e1a5677ced7603a
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2023-12-15 11:38:40 +0100

    x

--- scrip.js
+++ scrip.js
@@ -1,10 +1,38 @@
-function myfun(){
-    document.getElementById
-     ('form-wrapper').classList.add
-      ('active');
-   }
+
+// function myfun(){
+//     document.getElementById
+//      ('form-wrapper').classList.add
+//       ('active');
+//    }
   
-  function closer(){
-     document.getElementById('form-wrapper')
-    .classList.remove('active');
-  }
+//   function closer(){
+//      document.getElementById('form-wrapper')
+//     .classList.remove('active');
+//   }
+
+
+// const button = document.getElementById("#close");
+
+// button.addEventListener("click", (e) => {
+//   button.classList.toggle("container");
+// });
+
+// const btn = document.getElementById("#chat-now");
+
+// btn.addEventListener("click", (e) => {
+//     btn.classList.toggle("#form-wrapper");
+//   });
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
