9918b87c1ecbdacfc26d9790864d4ab70165040c
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2023-12-20 10:30:50 +0100

    form

---
+++ script.js
@@ -0,0 +1,17 @@
+const dialog = document.querySelector("dialog");
+
+const chat = document.getElementById("chat");
+
+chat.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+
+
+// const chatNow = document.getElementById("chat-now")
+
+// const container = document.querySelector(".container")
+ 
+// chatNow.addEventListener("click", () => {
+// container.style.setProperty("visibility", "visible");
+// });
